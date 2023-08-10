c2afb426a54acf7bc232557bca751bc1b14483d0
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-08-10 19:50:14 +0100

    interview challenge for starwars api manipulation and utf-8 validation tests.

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const filmNum = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmNum + '/';
+
+const getCharacterList = () => {
+	request(url, async function (err, response, body) {
+		if (err) return console.log('Fetch Error For Character URL');
+
+		const characterList = JSON.parse(body).characters;
+
+		const outChareList = []
+		for (const chUrl of characterList) {
+			let outChara = new Promise((resolve, reject) => {
+				request(chUrl, (err, response, body) => {
+					if (err) return console.log('Fetch Error For Character Data');
+					resolve(JSON.parse(body).name);
+				})
+			})
+			outChareList.push(outChara);
+		}
+
+		const data = await Promise.all(outChareList);
+		for (const name of data) {
+			console.log(name)
+		}
+	});
+}
+
+getCharacterList(url);
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,7 @@
+## API manipuation with JavaScript
+
+Write a script that prints all characters of a Star Wars movie:
+1) The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+1) Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+1) You must use the Star wars API
+1) You must use the request module
