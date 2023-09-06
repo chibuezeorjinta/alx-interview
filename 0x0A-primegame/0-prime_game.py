ed8721dc8ecad1916e1d2234f65af50f00e4e949
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-09-06 21:17:11 +0100

    Prime game solution

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+prime game predictor
+"""
+
+
+def isWinner(x, nums):
+    """
+    Returns the winner of a game over 'x' rounds
+    where each round works witht the corresponding
+    enter in the array 'nums'
+    Args:
+            x: int = number of rounds to perform
+            nums: list[int] = an array of int. each round works on the int
+            for that round
+    """
+    ben = 0
+    maria = 0
+    for y in range(x):
+        try:
+            n = nums[y]
+        except IndexError:
+            break
+        if n == 1:
+            ben = ben + 1
+            continue
+        if n == 0:
+            continue
+        count = 1  # non-prime count
+        for i in [2, n]:
+            for j in [i, n]:
+                if i * j > n:
+                    break
+                count = count + 1
+        primeNum = n - count
+        if primeNum % 2 == 0:
+            ben = ben + 1
+        else:
+            maria = maria + 1
+    if ben == maria:
+        return None
+    elif ben > maria:
+        return 'Ben'
+    elif ben < maria:
+        return 'Maria'
